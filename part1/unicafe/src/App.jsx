c83319897548baf722ca25dace825f1b9884cd50
justanotherrandomuser386 <119797935+justanotherrandomuser386@users.noreply.github.com>
2023-04-07 10:08:08 +0300

    1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -18,15 +18,22 @@ function App() {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+
+  let all = good + bad + neutral
+  let average = all != 0  ? (good - bad) / all : 0
+  let positive = all != 0 ? 100*good/all : 0
   return (
     <div>
       <Button handleClick={ () => {setGood(good + 1)} } name='good'/>
-      <Button handleClick={ () => {setNeutral(neutral + 1)} } name='good'/>
-      <Button handleClick={ () => {setBad(bad + 1)} } name='good'/>
+      <Button handleClick={ () => {setNeutral(neutral + 1)} } name='neutral'/>
+      <Button handleClick={ () => {setBad(bad + 1)} } name='bad'/>
 
       <Display label={'good'} value={good}/>
       <Display label={'neutral'} value={neutral}/>
       <Display label={'bad'} value={bad}/>
+      <Display label={'all'} value={all}/>
+      <Display label={'average'} value={average}/>
+      <Display label={'positive'} value={positive}/>
     </div>
   )
 }
