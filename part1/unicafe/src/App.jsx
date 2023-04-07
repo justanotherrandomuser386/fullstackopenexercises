5ffe72d6547758c97d28127985980a7a4cd6ffb2
justanotherrandomuser386 <119797935+justanotherrandomuser386@users.noreply.github.com>
2023-04-07 10:38:50 +0300

    1.9

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -18,13 +18,27 @@ const Statistics = ({good, neutral, bad}) => {
   const all = good + bad + neutral
   const average = all != 0  ? (good - bad) / all : 0
   const positive = all != 0 ? 100*good/all : 0
-  
-  return (
-    <>
-      <Display label={'all'} value={all}/>
-      <Display label={'average'} value={average}/>
-      <Display label={'positive'} value={positive}/>
-    </>
+ 
+  if (all == 0)
+    return (
+      <>
+        <h1>statistics</h1>
+        <div>
+          No feedback given
+        </div>
+      </>
+    )
+  else
+    return (
+      <>
+        <h1>statistics</h1>
+        <Display label={'good'} value={good}/>
+        <Display label={'neutral'} value={neutral}/>
+        <Display label={'bad'} value={bad}/>
+        <Display label={'all'} value={all}/>
+        <Display label={'average'} value={average}/>
+        <Display label={'positive'} value={positive}/>
+      </>
   )
    }
 
@@ -35,13 +49,10 @@ function App() {
 
   return (
     <div>
+      <h1>give feedback</h1>
       <Button handleClick={ () => {setGood(good + 1)} } name='good'/>
       <Button handleClick={ () => {setNeutral(neutral + 1)} } name='neutral'/>
       <Button handleClick={ () => {setBad(bad + 1)} } name='bad'/>
-
-      <Display label={'good'} value={good}/>
-      <Display label={'neutral'} value={neutral}/>
-      <Display label={'bad'} value={bad}/>
       <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
