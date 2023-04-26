f067f12d6f261fb6f4c5559940e34c7bb083f728
justanotherrandomuser386 <119797935+justanotherrandomuser386@users.noreply.github.com>
2023-04-26 16:50:37 +0300

    3.11

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -26,6 +26,7 @@ let persons = [
     }
 ]
 
+app.use(express.static('dist'))
 app.use(express.json())
 morgan.token('data', (request, response) => {return JSON.stringify(request.body)})
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
