824db3c2ca2741966eff5deb76f1e30337d52b18
justanotherrandomuser386 <119797935+justanotherrandomuser386@users.noreply.github.com>
2023-04-28 18:44:19 +0300

    3.12

---
+++ part3/phonebook/mongo.cjs
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+
+const password  = process.argv[2]
+const host      = process.argv[5] || '192.168.1.225:27017'
+const person    = process.argv[3]
+const number    = process.argv[4]
+const url = `mongodb://${host}/phonebook`
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  person: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (person === undefined) {
+  console.log('phonebook:')
+  Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log(`${person.person} ${person.number}`)
+    })
+  })
+  return mongoose.connection.close()
+} else {
+  const newPerson = new Person({
+    person,
+    number,
+  })
+  newPerson.save().then(() => {
+    console.log(`added ${person} number ${number} to phonebook`)
+    mongoose.connection.close()
+  })
+}
