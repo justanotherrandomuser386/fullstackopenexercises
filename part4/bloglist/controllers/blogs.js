8b8eb0db3f7aa215c4cde2ad54803cbf5b6b4fe9
justanotherrandomuser386 <119797935+justanotherrandomuser386@users.noreply.github.com>
2023-05-08 16:17:55 +0300

    4.14

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -28,4 +28,28 @@ blogsRouter.post('/', async (request, response, next) => {
   }
 })
 
+blogsRouter.delete('/:id', async (request, response, next) => {
+  try {
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+  } catch(exception) {
+    next(exception)
+  }
+})
+
+blogsRouter.put('/:id', async (request, response, next) => {
+  const blog = {...request.body}
+  try {
+    const b = await Blog.findById(request.params.id)
+    if (b === null) {
+      response.status(404).end()
+    }
+    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {new: true, runValidators: true, context: 'query'})
+    response.status(201).send(updatedBlog)
+  } catch(exception) {
+    next(exception)
+  }
+})
+
+
 module.exports = blogsRouter
--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -32,84 +32,170 @@ test('correct number of blogs returned', async () => {
 
 test('unique identifier of the blog is named id', async () => {
   const blogs = await helper.blogsInDB()
-  console.log(blogs)
   expect(blogs[0].id).toBeDefined()
 })
 
 test('unique identifier of the blog is not named _id', async () => {
   const blogs = await helper.blogsInDB()
-  console.log(blogs)
   expect(blogs[0]._id).not.toBeDefined()
 })
 
-test('new blog post can be added', async () => {
+describe('adding blog posts', () => {
+  test('new blog post can be added', async () => {
+      const newBlog = {
+          title: 'Go To Statement Considered Harmful',
+          author: 'Edsger W. Dijkstra',
+          url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+          likes: 5,
+        }
+
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+      
+      const blogs = await helper.blogsInDB()
+      expect(blogs.length).toBe(helper.initialBlogs.length + 1)
+      expect(blogs.map(blog => blog.title)).toContain(newBlog.title)
+  })
+
+  test('likes propery is defaults to 0', async () => {
+      const newBlog = {
+          title: 'Go To Statement Considered Harmful',
+          author: 'Edsger W. Dijkstra',
+          url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        }
+      
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+      
+      const blogs = await helper.blogsInDB()
+      expect(blogs.length).toBe(helper.initialBlogs.length + 1)
+      expect(blogs.filter(blog => blog.title === newBlog.title)[0].likes).toBe(0)
+      
+  })
+
+  test('new blog post without title can\'t be added', async () => {
+      const newBlog = {
+          author: 'Edsger W. Dijkstra',
+          url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+          likes: 5,
+        }
+
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(400)
+      
+      const blogs = await helper.blogsInDB()
+      expect(blogs.length).toBe(helper.initialBlogs.length)
+  })
+
+  test('new blog post without url can\'t be added', async () => {
+      const newBlog = {
+          title: 'Go To Statement Considered Harmful',
+          author: 'Edsger W. Dijkstra',
+          likes: 5,
+        }
+
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(400)
+
+      const blogs = await helper.blogsInDB()
+      expect(blogs.length).toBe(helper.initialBlogs.length)
+  })
+    
+})
+
+describe('deleting blog posts', () => {
+  test('deleting existing blog post', async () => {
     const newBlog = {
         title: 'Go To Statement Considered Harmful',
         author: 'Edsger W. Dijkstra',
         url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
         likes: 5,
       }
+    const savedBlog = await api.post('/api/blogs').send(newBlog)
+    let blogsAfter = await helper.blogsInDB()
+    expect(blogsAfter.length).toBe(helper.initialBlogs.length + 1)
 
     await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(201)
-      .expect('Content-Type', /application\/json/)
-    
-    const blogs = await helper.blogsInDB()
-    expect(blogs.length).toBe(helper.initialBlogs.length + 1)
-    expect(blogs.map(blog => blog.title)).toContain(newBlog.title)
-})
+      .delete(`/api/blogs/${savedBlog.body.id}`)
+      .expect(204)
 
-test('likes propery is defaults to 0', async () => {
+    blogsAfter = await helper.blogsInDB()
+    expect(blogsAfter.length).toBe(helper.initialBlogs.length)
+  })
+
+  test('deleting blog post wich does not exist', async () => {
     const newBlog = {
         title: 'Go To Statement Considered Harmful',
         author: 'Edsger W. Dijkstra',
         url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 5,
       }
-    
-  await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(201)
-      .expect('Content-Type', /application\/json/)
-    
-    const blogs = await helper.blogsInDB()
-    expect(blogs.length).toBe(helper.initialBlogs.length + 1)
-    expect(blogs.filter(blog => blog.title === newBlog.title)[0].likes).toBe(0)
-    
+    const savedBlog = await api.post('/api/blogs').send(newBlog)
+    let blogsAfter = await helper.blogsInDB()
+    expect(blogsAfter.length).toBe(helper.initialBlogs.length + 1)
+    await api
+      .delete(`/api/blogs/${savedBlog.body.id}`)
+      .expect(204)
+
+    await api
+      .delete(`/api/blogs/${savedBlog.body.id}`)
+      .expect(204)
+    blogsAfter = await helper.blogsInDB()
+    expect(blogsAfter.length).toBe(helper.initialBlogs.length)
+  })
 })
 
-test('new blog post without title can\'t be added', async () => {
+
+describe('updating blog posts', () => {
+  test('updating exisitng blog post', async () => {
     const newBlog = {
+        title: 'Go To Statement Considered Harmful',
         author: 'Edsger W. Dijkstra',
         url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
         likes: 5,
       }
-
-    await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(400)
     
-    const blogs = await helper.blogsInDB()
-    expect(blogs.length).toBe(helper.initialBlogs.length)
-})
+    const addedBlog = await api.post('/api/blogs').send(newBlog)
+    expect(addedBlog.body.url).toBe(newBlog.url)
 
-test('new blog post without url can\'t be added', async () => {
+    const editedBlog = {...newBlog}
+    editedBlog.url = 'null'
+
+    const updatedBlog = await api.put(`/api/blogs/${addedBlog.body.id}`).send(editedBlog)
+    expect(updatedBlog.body.id).toBe(addedBlog.body.id)
+    expect(updatedBlog.body.url).toBe(editedBlog.url)
+  })
+  
+  test('updating blog post which does not exist', async () => {
     const newBlog = {
         title: 'Go To Statement Considered Harmful',
         author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
         likes: 5,
       }
+    
+    const addedBlog = await api.post('/api/blogs').send(newBlog)
+    expect(addedBlog.body.url).toBe(newBlog.url)
+    await api.delete(`/api/blogs/${addedBlog.body.id}`).expect(204)
+    const editedBlog = {...newBlog}
+    editedBlog.url = 'null'
 
     await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(400)
+      .put(`/api/blogs/${addedBlog.body.id}`)
+      .send(editedBlog)
+      .expect(404)
+  })
 
-    const blogs = await helper.blogsInDB()
-    expect(blogs.length).toBe(helper.initialBlogs.length)
 })
 afterAll(async () => {
   await mongoose.connection.close()
